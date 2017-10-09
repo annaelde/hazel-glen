340edd2e204a2bfdde36034dce97cbea04e7ada4
annaelde <elde.av@gmail.com>
2017-10-09 00:25:13 -0400

    now works with urls with hashmark

--- js/navbar.js
+++ js/navbar.js
@@ -1,19 +1,18 @@
 // Highlights the active nav item.
 $(document).ready(function($) {
-    function markNavActive(){
-        var filename = window.location.href.substr(window.location.href.lastIndexOf("/")+1);
-        var listLinks = $('nav a');
-
-        listLinks.each(function() {
-            if ($(this).attr('href') == filename) {
-                $(this).addClass('active');
-                return;
-            }
-            if (filename == "") {
-                $('.nav a[href="index.php"]').addClass('active');
-                return;
-            }
-        });
-    }
-    markNavActive();    
-});
+    // Get filename
+    var filename = window.location.href.substr(window.location.href.lastIndexOf('/') + 1)
+    // Remove hash from filename
+    var filename = filename.replace(window.location.hash, '')
+    var navLinks = $('nav a')
+    // Iterate through nav links to find the active one
+    navLinks.each(function() {
+        if ($(this).attr('href') == filename) {
+            $(this).addClass('active')
+            return false
+        } else if (filename == '') {
+            $('.nav a[href="index.php"]').addClass('active')
+            return false
+        }
+    })
+})
